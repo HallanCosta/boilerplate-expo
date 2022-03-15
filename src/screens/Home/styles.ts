5fd9267cf3cd9e67003f070d79994e38835ced84
HallanCosta <hallan.costa1@hotmail.com>
2022-03-14 21:25:58 -0300

    feat(mobile): add stylesheet

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,12 @@
 import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
+export const styles = StyleSheet.create({
+  container: {
+
+  }
+});
+
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
