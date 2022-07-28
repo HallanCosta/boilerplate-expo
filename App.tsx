d55fca64c608121b1e79d9938ecf22c96c21af0a
HallanCosta <hallan.costa1@hotmail.com>
2022-07-28 18:49:32 -0300

    feat: add new alias @routes for index.tsx and @routes/* for others routes

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native';
 
-import { Routes } from './src/routes/routes';
+import { Routes } from '@routes';
 
 export default function App() {
   return (
